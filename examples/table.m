50c14abc27b509761fe521c605e7e28ccd9ee161
Bipin Nag <bipin.nag@gmail.com>
2021-09-10 19:03:13 +0530

    Update examples

---
+++ examples/table.m
@@ -0,0 +1,3 @@
+squares = Table[x^2, {x, 1, 10, 1}]
+
+cartesian = Flatten[Table[{x1, x2}, {x1, Range[5]}, {x2, Range[5]}], 1]
--- examples/uniform.m
+++ examples/uniform.m
@@ -1,5 +1,3 @@
-squares = Table[x^2, {x, 1, 10, 1}]
-
 data = RandomVariate[UniformDistribution[{1, 3}], 10^4]
 
 Show[Histogram[data, 20, "PDF"], Plot[PDF[UniformDistribution[{1, 3}],

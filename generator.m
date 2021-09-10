d09c11bfe248cbab18c43b05a23eec5b4bece3e4
Bipin Nag <bipin.nag@gmail.com>
2021-09-11 05:10:51 +0530

    Format generator.m

--- generator.m
+++ generator.m
@@ -1,25 +1,14 @@
 d = 32
-
 width = height = d
-
 EX = Table[{i, j} <-> {i + 1, j}, {i, width}, {j, width}]
-
 EY = Table[{i, j} <-> {i, j + 1}, {i, width}, {j, width}]
-
 p = 0.40
-
 q = 0.60
-
 Ex = RandomChoice[{p, 1 - p} -> {#, {}}]& /@ Flatten @ EX
-
 Ex = Cases[Except[{}]][Ex]
-
 Ey = RandomChoice[{q, 1 - q} -> {#, {}}]& /@ Flatten @ EY
-
 Ey = Cases[Except[{}]][Ey]
-
 g = Graph[Join[Ex, Ey]]
-
 walls =
     Table[
         {
@@ -40,5 +29,4 @@ walls =
         ,
         {j, height}
     ]; 
-
 Graphics[walls]
